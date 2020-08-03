133f29af62d8d371ad07ee993814f22d30e7c2a4
MarkoEspejo <42723963+mark0408@users.noreply.github.com>
2020-08-03 10:40:35 -0500

    terminando funcionalidad de consulta api StarWars

---
+++ src/planeta/aplication/llenarBaseDeDatos.js
@@ -0,0 +1,55 @@
+const { DynamoDB } = require('aws-sdk');
+const modelo = require('./../model/planeta');
+const dynamoDb = new DynamoDB.DocumentClient();
+
+async function obtenerPlanetas (event) {
+
+  let response ={};
+
+  for (let idInicial = 1; idInicial <= 10; idInicial++) {
+    let planeta = await obtenerPlanetasApiStarWars(idInicial);
+    
+    let planetaAtributoOrdenado = Object.entries(planeta).sort();
+    planetaAtributoOrdenado.push(["id",idInicial.toString()]);
+    let periodoRotacion = planetaAtributoOrdenado.sort().splice(11,1)
+    planetaAtributoOrdenado.splice(9,0,periodoRotacion[0])
+    let nombreDeCampos = Object.keys(modelo).sort();
+    let planetaAtribustoEspañol = [[]];
+    
+    for (let i=0; i <= 14; i++) {
+        planetaAtribustoEspañol.push([nombreDeCampos[i],planetaAtributoOrdenado[i][1]])
+        
+    }
+    planetaAtribustoEspañol.shift();
+    
+    let planetaEspañol = Object.fromEntries(planetaAtribustoEspañol);
+
+    const params = {
+        TableName: process.env.PLANETA_DYNAMODB_TABLE,
+        Item: planetaEspañol
+      }
+    response = dynamoDb.put(params)
+    .promise()
+    .then(() =>{
+      return {message: 'Registro exitoso'};
+    })
+    .catch(error =>{
+      return {message: 'erro en dynamoDb'}
+    }) 
+  }
+
+  return response;
+
+}
+
+async function obtenerPlanetasApiStarWars(idInicial){
+    const data = await fetch(`https://swapi.py4e.com/api/planets/${idInicial}/`)
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        return myJson
+      });
+    return data;
+}
+module.exports = obtenerPlanetas; 
